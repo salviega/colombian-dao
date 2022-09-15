00845b687725e1f1eabb888f7496fedd50522556
Santiago <salviega6@gmail.com>
2022-09-15 09:39:27 -0500

    [FEAT] Was added social network icons in the footer

--- src/components/MindFooter/index.js
+++ src/components/MindFooter/index.js
@@ -1,11 +1,25 @@
 import './MindFooter.scss'
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faTelegram } from '@fortawesome/free-brands-svg-icons'
 
 export function MindFooter() {
   return (
     <footer>
       <div className='footer-container'>
-        <p className='footer-container__content'> Mind connection... 🚧 </p>
+        <p className='footer-container__copyright'>© 2022 Colombian DAO</p>
+        <p className='footer-container__construction'> In construction... 🚧 </p>
+        <div className='footer-container__network'> 
+          <a href='/'>
+          <FontAwesomeIcon icon={faTelegram} />
+          </a>
+          <a href='https://www.linkedin.com/in/salviega/'>
+          <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href='https://github.com/salviega/colombian-dao'>
+          <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
       </div>
     </footer>
   )
--- src/components/MindMenu/index.js
+++ src/components/MindMenu/index.js
@@ -1,7 +1,7 @@
-import './MindMenu.scss'
-import logo from '../../asserts/images/mind-connection-logo.jpg'
+import './MindMenu.scss';
+import logo from '../../asserts/images/mind-connection-logo.jpg';
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 export function MindMenu() {
   return (
@@ -9,30 +9,139 @@ export function MindMenu() {
       <nav>
         <ul className='main-nav'>
           <li className='main-nav__item'>
-              <figure className='main-nav__logo'>
-                  <img src={logo} alt='logo'/>
-                  <figcaption>Mind Connection</figcaption>
-              </figure>
+            <figure className='main-nav__logo'>
+              <img src={logo} alt='logo' />
+              <figcaption>Colombian DAO</figcaption>
+            </figure>
           </li>
           <div className='main-nav-right'>
-            <li className='main-nav-right__item'>
-              <NavLink to='/about' className={({isActive}) => isActive ? 'main-nav-right__item--active' : ''}>About us</NavLink>
+            {routes.map((route, index) => (
+              <li key={index} className='main-nav-right__item'>
+              <NavLink
+                to={route.to}
+                className={({ isActive }) =>
+                  isActive ? 'main-nav-right__item--active' : ''
+                }
+              >
+                {route.web}
+              </NavLink>
+            </li>
+            ))}
+            { true && 
+                <li className='main-nav-right__button'>
+                  <button>Connect your wallet</button>
+                </li>
+            }
+            {/* <li className='main-nav-right__item'>
+              <NavLink
+                to='/about'
+                className={({ isActive }) =>
+                  isActive ? 'main-nav-right__item--active' : ''
+                }
+              >
+                About us
+              </NavLink>
             </li>
             <li className='main-nav-right__item'>
-              <NavLink to='/play' className={({isActive}) => isActive ? 'main-nav-right__item--active' : ''}>Play</NavLink>
+              <NavLink
+                to='/play'
+                className={({ isActive }) =>
+                  isActive ? 'main-nav-right__item--active' : ''
+                }
+              >
+                Play
+              </NavLink>
             </li>
             <li className='main-nav-right__item'>
-              <NavLink to='/login' className={({isActive}) => isActive ? 'main-nav-right__item--active' : ''}>Login</NavLink>
+              <NavLink
+                to='/login'
+                className={({ isActive }) =>
+                  isActive ? 'main-nav-right__item--active' : ''
+                }
+              >
+                Login
+              </NavLink>
             </li>
             <li className='main-nav-right__item'>
-              <NavLink to='/logout' className={({isActive}) => isActive ? 'main-nav-right__item--active' : ''}>Logout</NavLink>
+              <NavLink
+                to='/logout'
+                className={({ isActive }) =>
+                  isActive ? 'main-nav-right__item--active' : ''
+                }
+              >
+                Logout
+              </NavLink>
             </li>
-            <li className='main-nav-right__button'>
-              <button>Connect your wallet</button>
-            </li>
+             */}
           </div>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
+
+const routes = [
+  {
+    to:'/web2',
+    web: 'Web2',
+    routes: [
+      {
+        to: '/',
+        text: 'Home',
+        private: false,
+      },
+      {
+        to: 'about',
+        text: 'About US',
+        private: false,
+      },
+      {
+        to: '/play',
+        text: 'Play',
+        private: true,
+      },
+      {
+        to: '/profile',
+        text: 'Profile',
+        private: true,
+      },
+      {
+        to: '/login',
+        text: 'Login',
+        private: false,
+        publicOnly: true,
+      },
+      {
+        to: '/logout',
+        text: 'Logout',
+        private: true,
+      },
+    ],
+  },
+  {
+    to:'/web3',
+    web: 'Web3',
+    routes: [
+      {
+        to: '/',
+        text: 'Home',
+        private: false,
+      },
+      {
+        to: 'about',
+        text: 'About US',
+        private: false,
+      },
+      {
+        to: '/play',
+        text: 'Play',
+        private: true,
+      },
+      {
+        to: '/profile',
+        text: 'Profile',
+        private: true,
+      },
+    ],
+  },
+];
