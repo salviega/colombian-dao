9e50d8885924826faee9a9d05fd0a26d18e9f66e
Santiago <salviega6@gmail.com>
2022-09-18 12:44:14 -0500

    [STYLES] Was added styles in collection component

--- src/components/ColombianCollection/index.js
+++ src/components/ColombianCollection/index.js
@@ -8,7 +8,7 @@ import { getDataColombianSubGraph } from '../../middleware/getDataColombianSubGr
 export function ColombianCollection() {
   const { getItemsForSale } = getDataColombianSubGraph() 
   const [items, setItems] = React.useState([])
-  const [loading, setLoading] = React.useState(true)
+  const [loading, setLoading] = React.useState(false)
 
   const fetchData = async () => {
     setItems(await getItemsForSale())
@@ -27,18 +27,41 @@ export function ColombianCollection() {
             </figure>
           <h1 className='collection__title'>Explore NFTs</h1>
           <p className='collection__description'>{collection.description}</p>
+          
           { loading ? <div className='collection-container__loading'><ColombianLoading /></div>
-            : <div>
-              <h3 className='collection__subtitle'>Trending collections</h3>
-              <h3 className='collection__subtitle'>Trending collections</h3>
-              <h3 className='collection__subtitle'>Trending collections</h3>
-              <h3 className='collection__subtitle'>Trending collections</h3>
-              <h3 className='collection__subtitle'>Trending collections</h3>
-              <h3 className='collection__subtitle'>Trending collections</h3>
-              <h3 className='collection__subtitle'>Trending collections</h3>
+            : <div className='collection-cards'>
+              {
+                images.map((image, index) => (
+                  <>
+                    <div key={index} className='collection-cards__card'>
+                      <figure>
+                        <img src={image} alt='logo' />
+                      </figure>
+                      <div className='collection-cards__description'>
+
+                      </div>
+                    </div>
+                  </>
+                ))
+              }
             </div>
             }
         </div>
     </React.Fragment>
   )
 }
+
+const images = [
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2014%2F04%2Fgabriel-garcia-marquez.jpg%3Fquality%3D90%26strip%3Dall%26ssl%3D1&f=1&nofb=1",
+]
