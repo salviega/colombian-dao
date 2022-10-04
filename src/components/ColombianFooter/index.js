c3b9852319c238fb9f21cc551989c4d303766bac
Santiago <salviega6@gmail.com>
2022-10-04 09:13:41 -0500

    [STYLES] Was added styles in footer component

--- src/components/ColombianFooter/index.js
+++ src/components/ColombianFooter/index.js
@@ -1,25 +1,45 @@
 import './ColombianFooter.scss'
 import React from 'react';
+import logo from "../../asserts/images/colombian-dao-logo3.svg";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faTelegram } from '@fortawesome/free-brands-svg-icons'
+import { faTelegram, faLinkedinIn, faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export function ColombianFooter() {
   return (
     <footer>
       <div className='footer-container'>
-        <p className='footer-container__copyright'>© 2022 Colombian DAO</p>
-        <p className='footer-container__construction'> In construction... 🚧 </p>
+        <div className='footer-container__ads'>
+          <h1>Join our community</h1>
+        </div>
         <div className='footer-container__network'> 
           <a href='/'>
-          <FontAwesomeIcon icon={faTelegram} />
+            <div className='footer-container-icon'>
+              <FontAwesomeIcon icon={faTelegram} className='footer-container-icon__icon'/>
+            </div>
           </a>
           <a href='https://www.linkedin.com/in/salviega/'>
-          <FontAwesomeIcon icon={faLinkedin} />
+            <div className='footer-container-icon'>
+              <FontAwesomeIcon icon={faLinkedinIn} className='footer-container-icon__icon'/>
+            </div>
           </a>
           <a href='https://github.com/salviega/colombian-dao'>
-          <FontAwesomeIcon icon={faGithub} />
+          <div className='footer-container-icon'>
+              <FontAwesomeIcon icon={faDiscord} className='footer-container-icon__icon'/>
+            </div>
+          </a>
+          <a href='https://github.com/salviega/colombian-dao'>
+            <div className='footer-container-icon'>
+              <FontAwesomeIcon icon={faGithub} className='footer-container-icon__icon'/>
+            </div>
           </a>
         </div>
+      </div>
+      <div className='footer-container-copyright'>
+        <p className='footer-container-copyright__copyright'>©2022 CREA COLOMBIA</p>
+        <figure className='footer-container-copyright__construction'>
+          <img src={logo} alt='logo'/>
+        </figure>
       </div>
     </footer>
   )
