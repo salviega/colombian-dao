2fe58713151d15116b3d8ee074b7f0e191688bc9
Santiago <salviega6@gmail.com>
2022-10-05 05:56:56 -0500

    [STYLES] Was changed icons in footer component

--- src/components/ColombianFooter/index.js
+++ src/components/ColombianFooter/index.js
@@ -1,46 +1,57 @@
-import './ColombianFooter.scss'
-import React from 'react';
+import "./ColombianFooter.scss";
+import React from "react";
+import iTelegram from "../../asserts/images/icon-telegram.svg";
+import iLinkedin from "../../asserts/images/icon-linkedin.svg";
+import iDiscored from "../../asserts/images/icon-discored.svg";
+import iGithub from "../../asserts/images/icon-github.svg";
 import logo from "../../asserts/images/colombian-dao-logo3.svg";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTelegram, faLinkedinIn, faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTelegram,
+  faLinkedinIn,
+  faDiscord,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 export function ColombianFooter() {
   return (
     <footer>
-      <div className='footer-container'>
-        <div className='footer-container__ads'>
+      <div className="footer-container">
+        <div className="footer-container__ads">
           <h1>Join our community</h1>
         </div>
-        <div className='footer-container__network'> 
-          <a href='/'>
-            <div className='footer-container-icon'>
-              <FontAwesomeIcon icon={faTelegram} className='footer-container-icon__icon'/>
+        <div className="footer-container__network">
+          <a href="/">
+            <div className="footer-container-icon">
+                <img src={iGithub} alt="imagen" />
             </div>
           </a>
-          <a href='https://www.linkedin.com/in/salviega/'>
-            <div className='footer-container-icon'>
-              <FontAwesomeIcon icon={faLinkedinIn} className='footer-container-icon__icon'/>
+          <a href="https://www.linkedin.com/in/salviega/">
+            <div className="footer-container-icon">
+                <img src={iTelegram} alt="imagen" />
             </div>
           </a>
-          <a href='https://github.com/salviega/colombian-dao'>
-          <div className='footer-container-icon'>
-              <FontAwesomeIcon icon={faDiscord} className='footer-container-icon__icon'/>
+          <a href="https://github.com/salviega/colombian-dao">
+            <div className="footer-container-icon">
+                <img src={iLinkedin} alt="imagen" />
             </div>
           </a>
-          <a href='https://github.com/salviega/colombian-dao'>
-            <div className='footer-container-icon'>
-              <FontAwesomeIcon icon={faGithub} className='footer-container-icon__icon'/>
+          <a href="https://github.com/salviega/colombian-dao">
+            <div className="footer-container-icon">
+                <img src={iDiscored} alt="imagen" />
             </div>
           </a>
         </div>
       </div>
-      <div className='footer-container-copyright'>
-        <p className='footer-container-copyright__copyright'>©2022 CREA COLOMBIA</p>
-        <figure className='footer-container-copyright__construction'>
-          <img src={logo} alt='logo'/>
+      <div className="footer-container-copyright">
+        <p className="footer-container-copyright__copyright">
+          ©2022 CREA COLOMBIA
+        </p>
+        <figure className="footer-container-copyright__construction">
+          <img src={logo} alt="logo" />
         </figure>
       </div>
     </footer>
-  )
+  );
 }
